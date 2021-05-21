a943fae74d4a5d600b242620da91eadf1409479f
aggrimm <agrimmps@gmail.com>
2021-05-20 21:20:40 -0400

    Update BlueAccounting.js

--- js/BlueAccounting.js
+++ js/BlueAccounting.js
@@ -283,7 +283,16 @@ function BlueAccounting(){
                 min: 0,
                 opposite: true
             }],
-            
+            legend: {
+                symbolHeight: 11,
+                symbolWidth: 11,
+                symbolRadius: 0,
+                itemMarginBottom: 8,
+                itemStyle: {
+                    "word-wrap": "break-word",
+                    fontWeight: 'normal'
+                }
+            },
             caption: {
                 text: cap
             },
